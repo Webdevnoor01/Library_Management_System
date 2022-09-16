fa7a9be430b8f9548fb9213bb6a65a01d8f282b5
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-09-16 11:30:44 +0530

    created model folder

---
+++ backend/model/lAdmin.js
@@ -0,0 +1,37 @@
+const {Schema, model} = require("mongoose")
+
+/*
+Name
+User id
+Email
+avatar
+Phone
+Password
+*/
+const adminSchema = Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:Stirng,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+})
+
+const Admin = new model("Admin", adminSchema)
+
+module.exports Admin
