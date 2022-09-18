0b3fd14151933f620167627f4a2ab8cef0b09ad3
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-09-18 10:21:05 +0530

    The requested book model updated

--- backend/model/requestedBook.js
+++ backend/model/requestedBook.js
@@ -3,11 +3,16 @@ const { Schema, model, Types } = require("mongoose");
 const requestedBookSchema = Schema({
   userId: {
     type: Types.ObjectId,
+    references: [
+      { type: Types.ObjectId, ref: "Teacher" },
+      { type: Types.ObjectId, ref: "Students" },
+    ],
     required: true,
   },
   bookId: {
     type: Types.ObjectId,
     required: true,
+    ref: "Book",
   },
 });
 
