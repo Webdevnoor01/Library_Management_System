8c45d6bd22a8982b7e2c3793def6df4e263ef501
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-09-18 10:30:56 +0530

    lAdmin model renamed from Admin to libraryAdmin

--- backend/models/lAdmin.js
+++ backend/models/lAdmin.js
@@ -1,30 +1,28 @@
-const {Schema, model} = require("mongoose")
-
+const { Schema, model } = require("mongoose");
 
 const adminSchema = Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    userId:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:Stirng,
-        required:true
-    },
-    phone:{
-        type:Number,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: Stirng,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-})
+const libraryAdmin = new model("libraryAdmin", adminSchema);
 
-const Admin = new model("Admin", adminSchema)
-
-module.exports =Admin
+module.exports = libraryAdmin;
