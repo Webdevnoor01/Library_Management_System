66f6dc2434e264665f59436bc47703d28777a3dc
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-09-22 14:20:29 +0530

    change on app and route file

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,13 +1,28 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const router = require("./route")
 
 
 
-const app = express()
-app.use(router)
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
+const app = express()
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended:true}))
+app.use(router)
+
+
+app.use((err, req, res, next)=>{
+    if(err){
+        res.status(err.status || 500).json({
+            errors:{
+                common:{
+                    msg:err.message
+                }
+            }
+        })
+    }
+})
 
 
 module.exports = app
--- backend/app/route.js
+++ backend/app/route.js
@@ -5,4 +5,6 @@ router.get("/health",(req, res, next) =>{
     res.status(200).send()
 })
 
+router.use(require("../routes/index"))
+
 module.exports = router
