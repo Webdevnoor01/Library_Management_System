0aab78f704015c0b3964a8ed03e51dd68b771d05
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-09-22 14:17:06 +0530

    created route functionality for library card

--- backend/routes/libraryCard.js
+++ backend/routes/libraryCard.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const libraryCardValidation = require("../middleware/libraryCard/libraryCard")
+const libraryCardController = require("../controller/libraryCard/libraryCardController");
+
+
+router.post(
+"/create",
+libraryCardValidation.validator(),
+libraryCardValidation.validationHnadler,
+libraryCardController.create
+
+)
+router.get("/find", libraryCardController.findCard)
+
 
 module.exports = router;
