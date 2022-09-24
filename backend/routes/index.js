9091f4f604d9d2b14ca84ce300a521ae567a9128
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-09-24 12:31:23 +0530

    created router for auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,8 @@
-const router = require("express").Router()
-const libraryCardRoute= require("./libraryCard")
+const router = require("express").Router();
+const libraryCardRoute = require("./libraryCard");
+const authRoute = require("./authRoute");
 
-router.use("/api/v1/libraryCard",libraryCardRoute)
+router.use("/api/v1/libraryCard", libraryCardRoute);
+router.use("/api/v1/auth/s", authRoute);
 
-
-module.exports = router
+module.exports = router;
