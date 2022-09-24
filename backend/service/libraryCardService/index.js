e4a7e881c5ae011ace50054b1442d9bcbeeb2518
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-09-24 11:42:18 +0530

    Created a new service for update library card

--- backend/service/libraryCardService/index.js
+++ backend/service/libraryCardService/index.js
@@ -1,28 +1,48 @@
-const LibraryCard = require("../../models/libraryCard")
+const LibraryCard = require("../../models/libraryCard");
 
-
-class LibraryCardSearvice{
-
-    async createCard({userName, depertment, issuDate,libraryId, bookLimit}){
-        const libraryCard = await LibraryCard.create({
-            userName, depertment, issuDate, libraryId, bookLimit
-        })
-
-        return libraryCard;
+class LibraryCardSearvice {
+  async createCard({ userName, depertment, issueDate, libraryId, bookLimit }) {
+    const isLibraryCard = await this.findCardById(libraryId);
+    if (isLibraryCard) {
+      return false;
     }
+    const libraryCard = await LibraryCard.create({
+      userName,
+      depertment,
+      issueDate,
+      libraryId,
+      bookLimit,
+    });
 
-    async findCard(){
-        const libraryCards = await LibraryCard.find({})
-        return libraryCards
-    }
+    return libraryCard;
+  }
 
-    async findCardById(libraryId){
-        const libraryCard = await LibraryCard.findOne({libraryId:libraryId}).exec()
-        return libraryCard
+  async findCard() {
+    const libraryCards = await LibraryCard.find({});
+    return libraryCards;
+  }
+
+  async findCardById(libraryId) {
+    const libraryCard = await LibraryCard.findOne({
+      libraryId: libraryId,
+    });
+    console.log("libraryCard ", libraryCard);
+    return libraryCard;
+  }
+
+  async updateLibraryCard(libraryId, payload) {
+    try {
+      const libraryCard = await LibraryCard.findOneAndUpdate(
+        { libraryId: libraryId },
+        { ...payload }
+      );
+      return true;
+    } catch (e) {
+      return e;
     }
+  }
 }
 
+const libraryCardSearvice = new LibraryCardSearvice();
 
-const libraryCardSearvice = new LibraryCardSearvice()
-
-module.exports = libraryCardSearvice 
+module.exports = libraryCardSearvice;
