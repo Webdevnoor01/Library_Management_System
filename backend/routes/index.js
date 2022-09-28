5f2ba40f48da824f166be61478ed1676a0846b8d
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-09-28 11:15:49 +0530

    Changes in routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router()
 const libraryCardRoute= require("./libraryCard")
+const studentAuthRoute = require("./studentAuth")
 
 router.use("/api/v1/libraryCard",libraryCardRoute)
+router.use("/api/v1/auth",studentAuthRoute)
 
 
 module.exports = router
