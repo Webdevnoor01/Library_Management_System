874b511ca3fb4f5d4ec94e6e6070c6d0043db468
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-01 12:37:15 +0530

    created userService

---
+++ backend/service/userService/userService.js
@@ -0,0 +1,56 @@
+const libraryCardSearvice = require("../libraryCardService/index");
+
+class UserService {
+  async createNewUser(model, payload) {
+    try {
+      const user = await model.create({ ...payload });
+      return user;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async findUserByProperty(model, key, value, libraryId) {
+    try {
+      // First check that the library card is valid or not
+
+      const libraryCard = await libraryCardSearvice.findCardById(libraryId);
+      if (libraryCard) {
+        if (key === "_id") {
+          const user = await model.findById(value);
+
+          if (!user) return false;
+          if (
+            user.libraryId === libraryCard.libraryId &&
+            user.studentName === libraryCard.userName
+          ) {
+            return true;
+          } else {
+            throw new Error("Please type valid library id");
+          }
+        }
+
+        const user = await model.findOne({ key: value });
+
+        if (!user) {
+          if (user.libraryId == libraryCard.libraryId) {
+            if (user.studentName == libraryCard.userName) {
+              return true;
+            }
+          } else {
+            throw new Error("Please type valid library id");
+          }
+        } else {
+          throw new Error("Please try again ");
+        }
+      } else {
+        throw new Error("Please type valid library id");
+      }
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+  }
+}
+
+module.exports = new UserService();
