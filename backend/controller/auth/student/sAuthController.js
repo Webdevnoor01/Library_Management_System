4ce757de22f8b243bd21b1b7f0bc8672f230befc
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-02 12:51:12 +0530

    changes in student auth controller

--- backend/controller/auth/student/sAuthController.js
+++ backend/controller/auth/student/sAuthController.js
@@ -3,7 +3,7 @@ const userService = require("../../../service/userService/userService");
 const Student = require("../../../models/student");
 
 class AuthController {
-  async register(req, res, next) {
+  async register(req, res, _next) {
     const {
       studentName,
       email,
@@ -14,15 +14,15 @@ class AuthController {
       semester,
       admission_date,
       current_year,
+      userRole,
       address,
       libraryId,
     } = req.body;
 
     try {
-      const user = await userService.findUserByProperty(
+      const user = await userService.findUserByPropertyAndRegister(
         Student,
-        "libraryId",
-        libraryId,
+        { libraryId: libraryId },
         libraryId
       );
       if (!user) {
@@ -39,6 +39,7 @@ class AuthController {
           semester,
           admission_date,
           current_year,
+          userRole,
           address,
           libraryId,
         };
@@ -53,12 +54,56 @@ class AuthController {
         });
       } else {
         res.status(400).json({
-          message: "User already exists",
+          message: "Please try again",
         });
       }
     } catch (error) {
       res.status(400).json({
         message: "Please try again",
+      });
+    }
+  }
+
+  async login(req, res, next) {
+    const { email, password, role } = req.body;
+
+    try {
+      const user = await userService.findUserByProperty(Student, {
+        email: email,
+      });
+      if (!user) {
+        return res.status(400).json({
+          errors: {
+            login: {
+              msg: "Incorrect username and password",
+            },
+          },
+        });
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        return res.status(400).json({
+          errors: {
+            login: {
+              msg: "Incorrect username and password",
+            },
+          },
+        });
+      }
+      // TODO
+      // create token (access token and refresh token) and send token
+      res.status(200).json({
+        message: "Login successfully",
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        errors: {
+          common: {
+            msg: e.message,
+          },
+        },
       });
     }
   }
