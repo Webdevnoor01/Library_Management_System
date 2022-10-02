c5a4f3932823f371706de8904ef5b8f481fe7928
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-02 12:55:47 +0530

    changes in index route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,6 @@ const libraryCardRoute = require("./libraryCard");
 const studentAuthRoute = require("./studentAuth");
 
 router.use("/api/v1/libraryCard", libraryCardRoute);
-router.use("/api/v1/auth", studentAuthRoute);
+router.use("/api/v1/auth/student", studentAuthRoute);
 
 module.exports = router;
