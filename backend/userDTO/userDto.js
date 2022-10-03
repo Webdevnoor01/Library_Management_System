7fa3a928f0827e8e034ff77cba4df54c8e80ecdf
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-03 10:28:37 +0530

    created userDTO for create data object to send the user

---
+++ backend/userDTO/userDto.js
@@ -0,0 +1,17 @@
+class UserDto {
+    id;
+    phone;
+    name;
+    avatar;
+    createdAt;
+
+    constructor(user) {
+        this.id = user._id;
+        this.phone = user.phone;
+        this.name = user.studentName || user.teacherName || user.name;
+        this.avatar = user.avatar;
+        this.createdAt = user.createdAt;
+    }
+}
+
+module.exports = UserDto;
