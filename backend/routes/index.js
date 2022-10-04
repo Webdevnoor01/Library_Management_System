f12e292f6fbc6af380a1dc5b438c92a4eae4e6df
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-04 20:07:26 +0530

    Update index roure.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const libraryCardRoute = require("./libraryCard");
 const studentAuthRoute = require("./studentAuth");
+const teacherAuthRoute = require("./teacherAuth")
 
 router.use("/api/v1/libraryCard", libraryCardRoute);
 router.use("/api/v1/auth/student", studentAuthRoute);
+router.use("/api/v1/auth/teacher", teacherAuthRoute)
 
 module.exports = router;
