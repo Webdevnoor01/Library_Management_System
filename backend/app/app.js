fecb02f50ef46f6e34757d5364b6ec39cfb11fad
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-05 20:27:39 +0530

    Changes is app.

--- backend/app/app.js
+++ backend/app/app.js
@@ -7,9 +7,9 @@ const router = require("./route")
 
 
 const app = express()
-app.use(express.json())
 app.use(bodyParser.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true }))
+app.use(express.json())
 app.use(cookieParser())
 app.use(router)
 
