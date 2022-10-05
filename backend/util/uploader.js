db0396bdba658c9fe135ebd17df2ebd3f920859a
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-05 20:39:48 +0530

    Created uploader utility to upload image where included actual functionality for uploadin image.

---
+++ backend/util/uploader.js
@@ -0,0 +1,48 @@
+const multer = require("multer")
+const path = require("path")
+const createError = require("http-errors")
+function uploader(
+    sub_folder_path,
+    allow_file_type,
+    max_size,
+    err_msg
+){
+    const UPLOAD_FOLDER = `${__dirname}/../storage/${sub_folder_path}/`
+    const storage = multer.diskStorage({
+        destination:(_req,_file, cb) =>{
+            cb(null, UPLOAD_FOLDER)
+        },
+
+        filename: (_req, file, cb) =>{
+            const extName = path.extname(file.originalname)
+
+            let fileName = file.originalname
+                            .replace(extName, " ")
+                            .toLowerCase()
+                            .split(" ")
+                            .join("_") + Date.now();
+            
+            cb(null, fileName+extName)
+        }
+    })
+
+    const upload = multer({
+        storage: storage,
+        limits:{
+            fileSize: max_size
+        },
+        fileFilter:(_req, file, cb) =>{
+            if(allow_file_type.includes(file.mimetype)){
+                cb(null, true)
+            }else{
+                cb(createError(err_msg))
+            }
+        }
+    })
+
+    return upload;
+
+}
+
+
+module.exports = uploader
