3546983be3ec58993f37b43e868d76fff5814f63
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-05 20:29:13 +0530

    Add new route called book route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const libraryCardRoute = require("./libraryCard");
 const studentAuthRoute = require("./studentAuth");
 const teacherAuthRoute = require("./teacherAuth")
+const bookRoute = require("../routes/bookRoute")
 
 router.use("/api/v1/libraryCard", libraryCardRoute);
 router.use("/api/v1/auth/student", studentAuthRoute);
-router.use("/api/v1/auth/teacher", teacherAuthRoute)
+router.use("/api/v1/auth/teacher", teacherAuthRoute);
+router.use("/api/v1/book", bookRoute)
 
 module.exports = router;
