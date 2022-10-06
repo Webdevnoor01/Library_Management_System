799ad97a21d82b2753e0aaa94a23d311e9581157
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-06 12:51:11 +0530

    Add new service for book

--- backend/service/bookService/bookService.js
+++ backend/service/bookService/bookService.js
@@ -1,27 +1,46 @@
-const Book = require("../../models/books")
-const createError = require("http-errors")
+const Book = require("../../models/books");
+const createError = require("http-errors");
 
-class BookService{
-   async createNewBook(payload){
-        try {
-            const book  = await Book.create({...payload})
-            if(!book) return false
-            return book 
-        } catch (e) {
-            createError(e.message)
-        }
+class BookService {
+  async createNewBook(payload) {
+    try {
+      const book = await Book.create({ ...payload });
+      if (!book) return false;
+      return book;
+    } catch (e) {
+      throw createError(e.message);
     }
+  }
 
-    async findBookByIsbn(isbn){
-        try {
-            const isBook = await Book.findOne({isbn:isbn})
-            if(!isBook) return false
-            return isBook
-        } catch (e) {
-            createError(e.message)
-        }
+  async findBooks() {
+    try {
+      const books = await Book.find({});
+      if (!books.length > 0) return false;
+      return books;
+    } catch (e) {
+      throw createError(e.message);
     }
+  }
+
+  async findBookByProperty(query) {
+    try {
+      let book;
+      if (query.isbn && query.bookName && query.authorName) {
+        book = await Book.find({ ...query });
+      } else if (query.isbn) {
+        book = await Book.find({ isbn: query.isbn });
+      } else if (query.bookName) {
+        book = await Book.find({ bookName: query.bookName });
+      } else {
+        book = await Book.find({ authorName: query.authorName });
+      }
+
+      if (!book) return false;
+      return book;
+    } catch (e) {
+      throw createError(e.message);
+    }
+  }
 }
 
-
-module.exports = new BookService()
+module.exports = new BookService();
