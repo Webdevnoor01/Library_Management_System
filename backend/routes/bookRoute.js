64bb9f98d0a7534c46099edf7a3851728c99ee44
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-06 12:50:38 +0530

    Add new route for book.

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,15 +1,18 @@
-const router  = require("express").Router()
+const router = require("express").Router();
 
-const bookController = require("../controller/book/bookController")
-const bookValidator = require("../middleware/book/bookValidator")
-const imageUploader = require("../middleware/imageUploader/imageUploader")
+const bookController = require("../controller/book/bookController");
+const bookValidator = require("../middleware/book/bookValidator");
+const imageUploader = require("../middleware/imageUploader/imageUploader");
 
 router.post(
-    "/create",
-    imageUploader.uploadImage,
-    bookValidator.validate(),
-    bookValidator.validationHandler,
-    bookController.create
-     )
+  "/create",
+  imageUploader.uploadImage,
+  bookValidator.validate(),
+  bookValidator.validationHandler,
+  bookController.create
+);
 
-module.exports = router
+router.get("/find", bookController.findAllBooks);
+router.get("/findBy", bookController.findBookByProperty);
+
+module.exports = router;
