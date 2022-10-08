82e8cc7c27236a3c5ca3573be64c3bcaea38e746
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-08 11:32:37 +0530

    Created new server for book.

--- backend/service/bookService/bookService.js
+++ backend/service/bookService/bookService.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const Book = require("../../models/books");
 const createError = require("http-errors");
 const sendBookError = require("../../util/bookError");
@@ -23,51 +24,87 @@ class BookService {
     }
   }
 
-  async findBookByProperty(query) {
-    const { isbn, bookName, authorName } = query
+  async findBookByProperty(query, options = null) {
+    const { isbn, bookName, authorName } = query;
     try {
+      if (query._id) {
+        return (
+          (options && (await Book.findById({ ...query }, options))) ||
+          (await Book.findById({ ...query }))
+        );
+      }
       let book;
       if (isbn && bookName && authorName) {
         book = await Book.find({ ...query });
-        
-         book = book.length <=0 && sendBookError("Please type correct value.") || book
-        
+
+        book =
+          (book.length <= 0 && sendBookError("Please type correct value.")) ||
+          book;
       } else if (isbn && bookName) {
-        book = await Book.find({ isbn: isbn, bookName: bookName })
+        book = await Book.find({ isbn: isbn, bookName: bookName });
 
-        book = book.length <=0
-        && sendBookError("Please type right isbn & book name.")
-        || book;
-
+        book =
+          (book.length <= 0 &&
+            sendBookError("Please type right isbn & book name.")) ||
+          book;
       } else if (isbn && authorName) {
-        book = await Book.find({ isbn: isbn, authorName: authorName })
-        book = book.length <=0
-        && sendBookError("Please type right isbn & author name.")
-        || book;
+        book = await Book.find({ isbn: isbn, authorName: authorName });
+        book =
+          (book.length <= 0 &&
+            sendBookError("Please type right isbn & author name.")) ||
+          book;
       } else if (bookName && authorName) {
-        book = await Book.find({ bookName: bookName, authorName: authorName })
-        book = book.length <=0
-        && sendBookError("Please type right book name & author name.")
-        || book;
+        book = await Book.find({ bookName: bookName, authorName: authorName });
+        book =
+          (book.length <= 0 &&
+            sendBookError("Please type right book name & author name.")) ||
+          book;
       } else if (isbn) {
         book = await Book.find({ isbn: isbn });
-        book = book.length <=0
-        && sendBookError("Please check ISBN no.")
-        || book;
+        book =
+          (book.length <= 0 && sendBookError("Please check ISBN no.")) || book;
       } else if (bookName) {
         book = await Book.find({ bookName: bookName });
-        book = book.length <=0
-        && sendBookError("Please check book name.")
-        || book;
+        book =
+          (book.length <= 0 && sendBookError("Please check book name.")) ||
+          book;
       } else {
         book = await Book.find({ authorName: authorName });
-        book = book.length <=0
-        && sendBookError("Please check author name.")
-        || book;
+        book =
+          (book.length <= 0 && sendBookError("Please check author name.")) ||
+          book;
       }
       return book;
     } catch (e) {
-      console.log(e.message)
+      console.log(e.message);
+      throw createError(e.message);
+    }
+  }
+
+  async updateBook(id, payload) {
+    try {
+      if (payload.bookImage) {
+        const { bookImage } = await this.findBookByProperty(
+          { _id: id },
+          "bookImage"
+        );
+
+        if (!bookImage) throw createError("Error to uploading error");
+        await fs.unlink(bookImage, (err) => {
+          if (err) {
+            console.log(err);
+            throw createError("Error to upload image");
+          }
+        });
+      }
+
+      const book = await Book.findByIdAndUpdate(id, payload);
+      if (!book) {
+        return false;
+      }
+
+      return book;
+    } catch (e) {
       throw createError(e.message);
     }
   }
