d95b350914990e45dbf048063ff4315919139f6b
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-09 12:41:58 +0530

    update router.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const libraryCardRoute = require("./libraryCard");
 const studentAuthRoute = require("./studentAuth");
-const teacherAuthRoute = require("./teacherAuth")
-const bookRoute = require("../routes/bookRoute")
+const teacherAuthRoute = require("./teacherAuth");
+const bookRoute = require("../routes/bookRoute");
+const bookRequestRoute = require("../routes/bookRequestRoute");
 
 router.use("/api/v1/libraryCard", libraryCardRoute);
 router.use("/api/v1/auth/student", studentAuthRoute);
 router.use("/api/v1/auth/teacher", teacherAuthRoute);
-router.use("/api/v1/book", bookRoute)
+router.use("/api/v1/book", bookRoute);
+router.use("/api/v1/bookRequest", bookRequestRoute);
 
 module.exports = router;
