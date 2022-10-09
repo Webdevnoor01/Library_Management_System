cea2e16c35cd4b1a780bca6debea086d9aadb891
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761@gmail.com>
2022-10-09 12:43:48 +0530

    created new controller for requested book router.

---
+++ backend/controller/bookRequest/bookRequestController.js
@@ -0,0 +1,52 @@
+const createError = require("http-errors");
+const requestBookService = require("../../service/requestBookService/requestBookService");
+const userService = require("../../service/userService/userService");
+const Student = require("../../models/student");
+
+class BookRequest {
+  async newBookRequest(req, res) {
+    const { bookId, userId } = req.query;
+    try {
+      const payload = {
+        bookId,
+        userId,
+      };
+
+      const isRequestedBook = await requestBookService.findRequestedBook(
+        payload
+      );
+      if (isRequestedBook)
+        throw createError("You already send request for this book");
+
+      const requsetedBook = await requestBookService.newBookRequest(payload);
+      if (requsetedBook) {
+        const user = await userService.updateUserRef(
+          Student,
+          { _id: userId },
+          { requestedBookId: requsetedBook._id }
+        );
+        console.log("controller", user);
+
+        if (!user) {
+          throw createError("something went wrong");
+        }
+      }
+
+      if (!requsetedBook) throw createError("something went wrong.");
+      res.status(200).json({
+        message: "Ok",
+        requsetedBook,
+      });
+    } catch (e) {
+      res.status(500).json({
+        errors: {
+          requestedBook: {
+            msg: e.message,
+          },
+        },
+      });
+    }
+  }
+}
+
+module.exports = new BookRequest();
