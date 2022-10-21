e384f79fddc4f1a35294622934472313153a31c7
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-21 19:12:18 +0530

    Created new route for book request.

--- backend/routes/bookRequestRoute.js
+++ backend/routes/bookRequestRoute.js
@@ -1,8 +1,11 @@
 const bookRequestController = require("../controller/bookRequest/bookRequestController");
+const fequestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization")
 
 const router = require("express").Router();
 
 router.post("/", bookRequestController.newBookRequest);
 router.get("/find", bookRequestController.findRequestedBook)
+router.get("/find/user/:userId",fequestedBookAuthorization.authorized, bookRequestController.findUserRequestedBook)
+
 
 module.exports = router;
