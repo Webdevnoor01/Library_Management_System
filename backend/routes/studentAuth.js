ba8f9a2fe0bfe817357917985d7d9c5b4778c37c
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-27 10:57:42 +0530

    Changes in student auth route.

--- backend/routes/studentAuth.js
+++ backend/routes/studentAuth.js
@@ -2,15 +2,20 @@ const router = require("express").Router();
 const studentValidator = require("../middleware/auth/teacherStudent/teacherStudent");
 const studentController = require("../controller/auth/teacherStudent/tsAuthController");
 const authenticateMiddleware = require("../middleware/authenticate/authenticate")
+const requestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization")
 
+
+// Authentication related route
 router.post(
   "/register",
   studentValidator.validate(),
   studentValidator.validationHnadler,
   studentController.register
 );
-
 router.post("/login", studentController.login);
 router.post("/logout",authenticateMiddleware.authenticate, studentController.logout)
+
+// Library related route
+router.get("/issueBooks", authenticateMiddleware.authenticate, requestedBookAuthorization.authorized )
 
 module.exports = router;
