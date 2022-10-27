ec152ffedd0641b93ea9c62f91020b6c35e7c28f
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-27 11:00:39 +0530

    Update userDto.

--- backend/userDTO/userDto.js
+++ backend/userDTO/userDto.js
@@ -5,13 +5,14 @@ class UserDto {
     avatar;
     createdAt;
 
-    constructor(user) {
+    constructor(user,field) {
         this.id = user._id;
         this.phone = user.phone;
         this.name = user.studentName || user.teacherName || user.name;
         this.avatar = user.avatar;
         this.userRole = user.userRole
         this.createdAt = user.createdAt;
+        field = user[field]
     }
 }
 
