c16d6c39634a521265d7d5c6bcccf9db69aa710e
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-27 10:52:23 +0530

    Some changes in image uploader middleware.

--- backend/middleware/imageUploader/imageUploader.js
+++ backend/middleware/imageUploader/imageUploader.js
@@ -1,31 +1,36 @@
 const uploader = require("../../util/uploader");
 
 class ImageUploader {
-  uploadImage(req, res, next) {
-    try {
-      const upload = uploader(
-        "booksImg",
-        ["image/jpeg", "image/jpg", "image/png", "image/webp"],
-        4000000,
-        "Only .jpg, .jpeg or .png format allowed!"
-      );
+  
+  uploadImage(destination) {
 
-      upload.any()(req, res, (err) => {
-        if (err) {
-          res.status(400).json({
-            errors: {
-              bookImg: {
-                msg: err,
+    return (req, res, next) =>{
+      try {
+        const upload = uploader(
+          destination,
+          ["image/jpeg", "image/jpg", "image/png", "image/webp"],
+          2000000,
+          "Only .jpg, .jpeg or .png format allowed!"
+        );
+  
+        upload.any()(req, res, (err) => {
+          if (err) {
+            res.status(400).json({
+              errors: {
+                bookImg: {
+                  msg: err,
+                },
               },
-            },
-          });
-        } else {
-          next();
-        }
-      });
-    } catch (error) {
-      console.log(error);
+            });
+          } else {
+            next();
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
+    
   }
 }
 
