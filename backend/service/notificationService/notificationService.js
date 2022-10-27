8ce58fe7bc642c3346e1103307b2bdb240ffc741
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-27 11:06:13 +0530

    Created new service for notification service.

---
+++ backend/service/notificationService/notificationService.js
@@ -0,0 +1,37 @@
+// Libraries 
+const createError = require("http-errors")
+
+// Models
+const Notification = require("../../models/notification")
+
+
+class NotificationService {
+
+   async createNotification(payload){
+        try {
+            if(!(payload.message && payload.reciever && payload.sender)){
+                return {
+                    error:true,
+                    message:"Please type valid message reciever sender"
+                }
+            }
+            const notification = await Notification.create(payload)
+            if(!notification){
+                return {
+                    error:true,
+                    message:"error to creating notification"
+                }
+            }
+
+            return {
+                error:false,
+                data:notification
+            }
+        } catch (e) {
+            throw createError(e.message)
+        }
+    }
+
+}
+
+module.exports = new NotificationService()
