ed19cd2dd044a4d8d5746a69015b58271c324a17
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-27 11:07:06 +0530

    Created new service for profile service.

---
+++ backend/service/profileService/profileService.js
@@ -0,0 +1,24 @@
+const createError = require("http-errors")
+
+class ProfileService {
+
+    async update(model, payload,userId){
+        try {
+            const profile = await model.findByIdAndUpdate(userId, payload)
+            if(!profile) {
+                return {
+                    error:true,
+                    message:"Error to update profile"
+                }
+            }
+            return {
+                error:false,
+                data:profile
+            }
+        } catch (e) {
+            throw createError(e.message)
+        }
+    }
+}
+
+module.exports = new ProfileService()
