6ec5595df349cbf0d9519ed22882ca3c097eca0c
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-28 15:18:20 +0530

    Added new route in studentRoute route.

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -2,7 +2,9 @@ const router = require("express").Router()
 const studentController = require("../controller/student/studentController")
 const requestedBookAuthorization  = require("../middleware/authorization/requestedBookAuthorization")
 
-
+router.get("/find",studentController.findStudents)
 router.get("/issue_books/:userId", requestedBookAuthorization.authorized, studentController.findRequestedBook )
+router.get("/notifications", studentController.getNotification )
+router.delete("/reject_issue_book", requestedBookAuthorization.authorized, studentController.deleteRequestedBook )
 
 module.exports = router
