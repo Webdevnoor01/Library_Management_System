06fe14833976deb6419594fc3bb13b6948c1bd1a
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-28 15:22:19 +0530

    Update findModel.

--- backend/util/findModel.js
+++ backend/util/findModel.js
@@ -1,5 +1,7 @@
 const Student = require("../models/student")
 const Teacher = require("../models/teacher")
+const LibAdmin = require("../models/lAdmin")
+const LibraryStaff = require("../models/libraryStaff")
 
 
 function findModel(modelName){
@@ -7,10 +9,18 @@ function findModel(modelName){
         return Student
     }
     
-    if(modelName == "Teacher" || modelName === "teacher"){
+    if(modelName === "Teacher" || modelName === "teacher"){
         
         return Teacher
     }
+
+    if(modelName === "libAdmin" || modelName === "LibAdmin"){
+        return LibAdmin
+    }
+    if(modelName === "Assistant" || modelName === "assistant" || modelName === "Staff" || modelName === "staff"){
+        return LibraryStaff
+    }
+
 }
 
 module.exports = findModel
