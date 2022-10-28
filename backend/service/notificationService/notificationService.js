3bebf55e5d7ed9ebbb6e2faf1bc7e9102cee620d
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-28 15:19:21 +0530

    Added new service in notification service.

--- backend/service/notificationService/notificationService.js
+++ backend/service/notificationService/notificationService.js
@@ -15,11 +15,37 @@ class NotificationService {
                     message:"Please type valid message reciever sender"
                 }
             }
+
             const notification = await Notification.create(payload)
             if(!notification){
                 return {
                     error:true,
                     message:"error to creating notification"
+                }
+            }
+
+            return {
+                error:false,
+                data:notification
+            }
+        } catch (e) {
+            throw createError(e.message)
+        }
+    }
+
+    /**
+     * 
+     * @param {Object} query {reciever:
+     *  {userId, role, userName}
+     * }
+     */
+    async findNotificationByProperty(query){
+        try {
+            const notification = await Notification.find(query)
+            if(!notification.length >0 ){
+                return {
+                    error:true,
+                    message:"Empty notification"
                 }
             }
 
