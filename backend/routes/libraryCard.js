011192171d094cd745f57d32e03ba884ee7b8514
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-30 13:04:06 +0530

    Update library route .

--- backend/routes/libraryCard.js
+++ backend/routes/libraryCard.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 const libraryCardValidation = require("../middleware/libraryCard/libraryCard");
 const libraryCardController = require("../controller/libraryCard/libraryCardController");
-const libraryCardSearvice = require("../service/libraryCardService");
-
+const adminAuthorization = require("../middleware/authorization/adminAuthorization")
 router.post(
   "/create",
+  adminAuthorization.authorized,
   libraryCardValidation.validator(),
   libraryCardValidation.validationHnadler,
   libraryCardController.create
 );
+
 router.get("/find", libraryCardController.findCard);
 router.get("/find/:libraryId", libraryCardController.findCardByLid);
-router.patch("/update/:libraryId", libraryCardController.uadateCard);
+
+router.patch("/update/:libraryId",adminAuthorization.authorized, libraryCardController.uadateCard);
 
 module.exports = router;
