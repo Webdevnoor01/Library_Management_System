d190f82f0e255b1f5867ad7a5f1198225f7de2bb
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-10-30 13:15:35 +0530

    Created new service for libraryAdminService.

---
+++ backend/service/libraryAdminService/libraryAdminService.js
@@ -0,0 +1,67 @@
+const LibraryAdmin = require("../../models/lAdmin")
+const IssuedBook = require("../../models/issuedBook")
+const createError = require("http-errors")
+
+
+
+class LibraryAdminService {
+    async create(payload){
+        try {
+            const libAdmin = await LibraryAdmin.create(payload)
+            if(!libAdmin){
+                return {
+                    error:true,
+                    message:libAdmin
+                }
+            }
+
+            return {
+                error:false,
+                data:libAdmin
+            }
+        } catch (e) {
+            throw createError(e.message)
+        }
+    }
+
+    async findByProperty(query){
+        try {
+            let libAdmin;
+            if(query._id){
+                libAdmin = await LibraryAdmin.findById(query._id)
+                if(!libAdmin){
+                    return {
+                        error:true,
+                        message:libAdmin
+                    }
+                }
+
+                return {
+                    error:false,
+                    message:libAdmin
+                }
+            }
+
+            libAdmin = await LibraryAdmin.findOne(query)
+
+            if(!libAdmin){
+                return {
+                    error:true,
+                    message:libAdmin
+                }
+            }
+
+            return {
+                error:false,
+                message:libAdmin
+            }
+        } catch (e) {
+            console.log(e)
+            throw createError(e.message)
+        }
+    }
+
+   
+}
+
+module.exports = new LibraryAdminService()
