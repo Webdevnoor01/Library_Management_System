82c3bd2e0fffb23e5169e9930b7c7e6866a22510
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-11-25 19:58:06 +0530

    Some changes in studen route.

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 const studentController = require("../controller/student/studentController")
 const requestedBookAuthorization  = require("../middleware/authorization/requestedBookAuthorization")
+const studentAuthorization = require("../middleware/authorization/studentAuthorization")
+const authenticateMiddleware = require("../middleware/authenticate/authenticate")
 
 router.get("/find",studentController.findStudents)
 router.get("/issue_books/:userId", requestedBookAuthorization.authorized, studentController.findRequestedBook )
-router.get("/notifications", studentController.getNotification )
+router.get("/notifications",authenticateMiddleware.authenticate,studentAuthorization.authorized, studentController.getNotification )
 router.delete("/reject_issue_book", requestedBookAuthorization.authorized, studentController.deleteRequestedBook )
 
 module.exports = router
