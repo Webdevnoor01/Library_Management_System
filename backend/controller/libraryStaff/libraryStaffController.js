1359ac3330241d229e063879a529586738a84f08
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-11-26 19:09:42 +0530

    Created new controller fo rlibaray staff.

---
+++ backend/controller/libraryStaff/libraryStaffController.js
@@ -0,0 +1,52 @@
+const createError = require("http-errors")
+const libraryStaffService = require("../../service/libraryStaffService/libraryStaffService")
+const bcrypt = require("bcrypt")
+class LibraryStaffController {
+    async register(req, res){
+        const {name, email, phone, password, userRole } = req.body
+        try {
+            const hashPassword = await bcrypt.hash(password, 10)
+            const payload = {
+                name,
+                email,
+                phone,
+                password:hashPassword,
+                userRole
+            }
+
+            const isLibraryStaff = await libraryStaffService.findLibraryStaffByProperty("email", email)
+            console.log(isLibraryStaff.data)
+            if(!isLibraryStaff.error){
+                throw createError({
+                    message:{
+                        status:400,
+                        msg:`${userRole} already exist`
+                    }
+                })
+            }
+            const libraryStaff = await libraryStaffService.create(payload)
+            console.log(libraryStaff)
+            if(libraryStaff.error){
+                throw createError({
+                    message:libraryStaff.message
+                })
+            }
+            res.status(200).json({
+                message:`${userRole} has been created`,
+                data:libraryStaff.data
+            })
+        } catch (e) {
+            res.status(e.message.status || 500).json({
+                errors:{
+                    libraryStaff:{
+                        msg:e.message.msg
+                    }
+                }
+            })
+        }
+    }
+
+   
+}
+
+module.exports = new LibraryStaffController()
