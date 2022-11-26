52161e9d6f8a1b51658fec51c6056a9e7ae50440
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-11-26 19:07:58 +0530

    Update library staff model.

--- backend/models/libraryStaff.js
+++ backend/models/libraryStaff.js
@@ -10,6 +10,10 @@ const libraryStaffSchema = Schema(
       type: String,
       required: true,
     },
+    phone:{
+      type:Number,
+      required:true
+    },
     password: {
       type: String,
       required: true,
