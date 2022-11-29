609feed7abfbb321da63edf0c3254ee90a4836bd
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-11-29 11:08:00 +0530

    Updated library staff service.

--- backend/service/libraryStaffService/libraryStaffService.js
+++ backend/service/libraryStaffService/libraryStaffService.js
@@ -1,50 +1,99 @@
-const LibraryStaff = require("../../models/libraryStaff")
-const createError = require("http-errors")
+const LibraryStaff = require("../../models/libraryStaff");
+const createError = require("http-errors");
+const UserDto = require("../../userDTO/userDto");
 
 class LibraryStaffService {
-    async create(payload){
-        try {
-            const libraryStaff = await LibraryStaff.create(payload)
-            console.log("LibraryStaff: ", libraryStaff)
-            if(!libraryStaff){
-                return {
-                    error:true,
-                    message:"There was a problem to create new staff"
-                }
-            }
-            return{
-                error:false,
-                data:libraryStaff
-            }
-        } catch (e) {
-            console.log(e)
-            throw createError({
-                message:e.message
-            })
-        }
+  async create(payload) {
+    try {
+      const libraryStaff = await LibraryStaff.create(payload);
+      if (!libraryStaff) {
+        return {
+          error: true,
+          message: "There was a problem to create new staff",
+        };
+      }
+      return {
+        error: false,
+        data: libraryStaff,
+      };
+    } catch (e) {
+      console.log(e);
+      throw createError({
+        message: e.message,
+      });
     }
+  }
 
-    async findLibraryStaffByProperty(key, value){
-        try {
-            const libraryStaff = await LibraryStaff.findOne({key:value}, "name email phone userRole")
-            if(!libraryStaff){
-                return{
-                    error:true,
-                    message:"Library staff not found"
-                }
-            }
+  async findLibraryStaffByProperty(query) {
+    try {
+      let libraryStaff;
+      if (query) {
+        if (query.id) {
+          libraryStaff = await LibraryStaff.findById(query.id, "name email phone userRole avatar");
+        }
+      } else if (query === undefined) {
+        libraryStaff = await LibraryStaff.find({},"name email phone userRole avatar");
+      } else {
+        libraryStaff = await LibraryStaff.findOne(
+          query,
+          "name email phone userRole"
+        );
+      }
+
+      if (!libraryStaff) {
+        return {
+          error: true,
+          message: "Library staff not found",
+        };
+      }
+      return {
+        error: false,
+        data: libraryStaff,
+      };
+    } catch (e) {
+      throw createError({
+        message: e.message,
+      });
+    }
+  }
+
+  async delete(staffId) {
+    try {
+      const staff = await LibraryStaff.findByIdAndRemove(staffId);
+      if (!staff) {
+        return {
+          error: true,
+          message: "There was a errro to delete staff",
+        };
+      }
+
+      return {
+        error: false,
+        message: staff,
+      };
+    } catch (e) {
+      throw createError(e.message);
+    }
+  }
+
+  async update(staffId, payload){
+    try {
+        console.log(payload)
+        const staff = await LibraryStaff.findByIdAndUpdate(staffId, payload)
+        if(!staff){
             return {
-                error:false,
-                data:libraryStaff
+                error:true,
+                message:"There was a problem to updta this staff"
             }
-        } catch (e) {
-            throw createError({
-                message:e.message
-            })
         }
+        return {
+            error:false,
+            data:new UserDto(staff)
+        }
+    } catch (e) {
+        throw createError(e.message)
     }
-
-    
+  }
 }
 
-module.exports = new LibraryStaffService
+module.exports = new LibraryStaffService();
