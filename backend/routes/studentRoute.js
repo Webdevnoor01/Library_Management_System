332784078175fe033483b3a0b571243d2375da76
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-21 10:50:34 +0530

    Update student route.

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -6,6 +6,8 @@ const authenticateMiddleware = require("../middleware/authenticate/authenticate"
 
 router.get("/find",studentController.findStudents)
 router.get("/issue_books/:userId", requestedBookAuthorization.authorized, studentController.findRequestedBook )
+router.get("/find/issued_book", authenticateMiddleware.authenticate,
+requestedBookAuthorization.authorized, studentController.findIssuedBooks)
 router.get("/notifications",authenticateMiddleware.authenticate,studentAuthorization.authorized, studentController.getNotification )
 router.delete("/reject_issue_book", requestedBookAuthorization.authorized, studentController.deleteRequestedBook )
 
