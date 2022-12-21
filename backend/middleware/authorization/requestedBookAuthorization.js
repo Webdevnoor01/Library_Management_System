ad275922e0d108fca26989d6527e38299bb1bb4d
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-21 10:44:31 +0530

    Some changes in requested book authorization middleware.

--- backend/middleware/authorization/requestedBookAuthorization.js
+++ backend/middleware/authorization/requestedBookAuthorization.js
@@ -4,11 +4,9 @@ class RequestedBookAuthorization {
 
    async authorized(req, res, next) {
        const {accessToken} = req.cookies
-       const userId = req.params.userId || req.query.userId
-       console.log("requestedBookAuthorizaion: ", userId)
+       const userId = req.params.userId ? req.params.userId:req.query.userId;
         try {
             const user = await jwt.verify(accessToken, process.env.JWT_ACCES_TOKEN_SECRET)
-            console.log("authorization: ", user)
             if(user._id !== userId){
                 throw createError("Unauthorized access")
             }            
