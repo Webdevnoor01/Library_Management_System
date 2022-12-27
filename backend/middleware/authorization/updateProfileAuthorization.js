78518720a977b0cde240bfdcbbcd1e0471e0fc5a
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-27 11:25:43 +0530

    Create new authorization for profile update authorization.

---
+++ backend/middleware/authorization/updateProfileAuthorization.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken")
+const createError = require("http-errors")
+class UpdateProfileAuthorization {
+
+   async authorized(req, res, next) {
+       const {accessToken} = req.cookies
+       const userId = req.params.userId || req.query.userId || req.user._id
+        try {
+            const user = await jwt.verify(accessToken, process.env.JWT_ACCES_TOKEN_SECRET)
+            if(user._id !== userId){
+                throw createError("Unauthorized access")
+            }            
+            req.userRole = user.userRole
+        next()
+        } catch (e) {
+            res.status(400).json({
+                errors:{
+                    authorization:{
+                        msg:e.message
+                    }
+                }
+            })
+        }
+        
+    }
+
+
+}
+
+module.exports = new UpdateProfileAuthorization()
