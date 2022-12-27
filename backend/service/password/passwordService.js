119bb7574facb6a7f3c578d7e32ccd3d0c5fb579
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-27 11:27:35 +0530

    Created new service for password.

---
+++ backend/service/password/passwordService.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt")
+const createError = require("http-errors")
+
+class PasswordService{
+
+//   user this serviece for forgot password
+}
+
+module.exports = new PasswordService()
