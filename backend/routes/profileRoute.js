3de10aa2263571fee7573946945a23e129095c70
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-27 11:23:54 +0530

    Update profile route.

--- backend/routes/profileRoute.js
+++ backend/routes/profileRoute.js
@@ -1,11 +1,18 @@
-const router= require("express").Router()
-const profileController = require("../controller/profile/profileController")
-const imageUploader = require("../middleware/imageUploader/imageUploader")
+const router = require("express").Router();
+const profileController = require("../controller/profile/profileController");
+const imageUploader = require("../middleware/imageUploader/imageUploader");
+const updateProfileAuthorization = require("../middleware/authorization/updateProfileAuthorization");
 
+router.patch(
+  "/update/:userId",
+  updateProfileAuthorization.authorized,
+  profileController.update
+);
+router.patch(
+  "/update/avatar/:userId",
+  updateProfileAuthorization.authorized,
+  imageUploader.uploadImage("userAvatar"),
+  profileController.update
+);
 
-
-
-router.patch("/update",profileController.update)
-router.patch("/update/avatar",imageUploader.uploadImage("userAvatar"), profileController.update)
-
-module.exports = router
+module.exports = router;
