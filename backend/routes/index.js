d1396808526ce97dd729c4518b1d3feae5af80a4
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-27 11:23:31 +0530

    Update index route.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,8 +8,10 @@ const teacherRoute = require("./teacherRoute")
 const bookRoute = require("../routes/bookRoute");
 const bookRequestRoute = require("../routes/bookRequestRoute");
 const profileRouter = require("./profileRoute")
-const authenticateMiddleware = require("../middleware/authenticate/authenticate");
 const libraryAssistantRoute = require("../routes/libraryAssistantRoute")
+const passwordRouter = require("../routes/password")
+const authenticateMiddleware = require("../middleware/authenticate/authenticate");
+
 
 
 // Admin Routes
@@ -31,5 +33,7 @@ router.use("/api/v1/book",authenticateMiddleware.authenticate, bookRoute);
 router.use("/api/v1/bookRequest",authenticateMiddleware.authenticate,  bookRequestRoute);
 
 router.use("/api/v1/profile",authenticateMiddleware.authenticate, profileRouter )
+
+router.use("/api/v1/password", authenticateMiddleware.authenticate, passwordRouter)
 
 module.exports = router;
