34e8c3e502592ed13f87ec3d9b6b18078f7d5eea
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-31 15:39:19 +0530

    Updated teacher route.

--- backend/routes/teacherRoute.js
+++ backend/routes/teacherRoute.js
@@ -1,9 +1,7 @@
 const requestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization");
-const authenticateMiddleware = require("../middleware/authenticate/authenticate")
+const authenticateMiddleware = require("../middleware/authenticate/authenticate");
 const teacherController = require("../controller/student/studentController");
 const router = require("express").Router();
-
-
 
 router.get("/find", teacherController.findStudents);
 router.get(
@@ -12,9 +10,18 @@ router.get(
   requestedBookAuthorization.authorized,
   teacherController.findRequestedBook
 );
-router.get("/find/issued_book/:userId",authenticateMiddleware.authenticate,
-requestedBookAuthorization.authorized, teacherController.findIssuedBooks);
-
+router.get(
+  "/find/issued_book/:userId",
+  authenticateMiddleware.authenticate,
+  requestedBookAuthorization.authorized,
+  teacherController.findIssuedBooks
+);
+router.get(
+  "/find/returned-books/:userId",
+  authenticateMiddleware.authenticate,
+  requestedBookAuthorization.authorized,
+  teacherController.findReturnedBook
+);
 router.get(
   "/notifications/:userId",
   authenticateMiddleware.authenticate,
@@ -25,6 +32,12 @@ router.delete(
   "/reject_request_book",
   requestedBookAuthorization.authorized,
   teacherController.deleteRequestedBook
+);
+
+router.post(
+  "/return-book/:userId",
+  requestedBookAuthorization.authorized,
+  teacherController.returnBook
 );
 
 module.exports = router;
