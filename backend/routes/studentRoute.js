3784ba044ee4879d925ed40c51076fd74685ea13
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-31 15:38:57 +0530

    Updated student route.

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,14 +1,43 @@
-const router = require("express").Router()
-const studentController = require("../controller/student/studentController")
-const requestedBookAuthorization  = require("../middleware/authorization/requestedBookAuthorization")
-const studentAuthorization = require("../middleware/authorization/studentAuthorization")
-const authenticateMiddleware = require("../middleware/authenticate/authenticate")
+const router = require("express").Router();
+const studentController = require("../controller/student/studentController");
+const requestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization");
+const studentAuthorization = require("../middleware/authorization/studentAuthorization");
+const authenticateMiddleware = require("../middleware/authenticate/authenticate");
 
-router.get("/find",studentController.findStudents)
-router.get("/issue_books/:userId", requestedBookAuthorization.authorized, studentController.findRequestedBook )
-router.get("/find/issued_book", authenticateMiddleware.authenticate,
-requestedBookAuthorization.authorized, studentController.findIssuedBooks)
-router.get("/notifications",authenticateMiddleware.authenticate,studentAuthorization.authorized, studentController.getNotification )
-router.delete("/reject_issue_book", requestedBookAuthorization.authorized, studentController.deleteRequestedBook )
+router.get("/find", studentController.findStudents);
+router.get(
+  "/issue_books/:userId",
+  requestedBookAuthorization.authorized,
+  studentController.findRequestedBook
+);
+router.get(
+  "/find/issued_book/:userId",
+  authenticateMiddleware.authenticate,
+  requestedBookAuthorization.authorized,
+  studentController.findIssuedBooks
+);
+router.get(
+  "/find/returned-books/:userId",
+  authenticateMiddleware.authenticate,
+  requestedBookAuthorization.authorized,
+  studentController.findReturnedBook
+);
+router.get(
+  "/notifications",
+  authenticateMiddleware.authenticate,
+  studentAuthorization.authorized,
+  studentController.getNotification
+);
+router.delete(
+  "/reject_issue_book/:userId",
+  requestedBookAuthorization.authorized,
+  studentController.deleteRequestedBook
+);
 
-module.exports = router
+router.post(
+  "/return-book/:userId",
+  requestedBookAuthorization.authorized,
+  studentController.returnBook
+);
+
+module.exports = router;
