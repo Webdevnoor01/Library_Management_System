3bdae9ded4b3d7a015377fab5bc05d44af226e5e
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-12-31 15:38:31 +0530

    Update library amdin route.

--- backend/routes/libraryAdminRoute.js
+++ backend/routes/libraryAdminRoute.js
@@ -1,64 +1,93 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
-const libraryAdmiinValidator = require("../middleware/libraryAdmin/libraryAdminValidator")
+const libraryAdmiinValidator = require("../middleware/libraryAdmin/libraryAdminValidator");
 
-const LibraryAdminController = require("../controller/libraryAdmin/libraryAdminController")
-const libraryAdminController = require("../controller/libraryAdmin/libraryAdminController")
+const LibraryAdminController = require("../controller/libraryAdmin/libraryAdminController");
+const libraryAdminController = require("../controller/libraryAdmin/libraryAdminController");
 
-const authController = require("../controller/auth/teacherStudent/tsAuthController")
-const adminAuthorization = require("../middleware/authorization/adminAuthorization")
-const authenticateMiddleware  = require("../middleware/authenticate/authenticate")
+const authController = require("../controller/auth/teacherStudent/tsAuthController");
+const adminAuthorization = require("../middleware/authorization/adminAuthorization");
+const authenticateMiddleware = require("../middleware/authenticate/authenticate");
 
-const libraryStaffValidator = require("../middleware/libraryStaff/libraryStaffValidator")
-const libraryStaffControler = require("../controller/libraryStaff/libraryStaffController")
-const libraryStaffService = require("../service/libraryStaffService/libraryStaffService")
-
-
-router.post(
-    "/create",
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    libraryAdmiinValidator.validate(),
-    libraryAdmiinValidator.validationHnadler,
-    libraryAdminController.createAdmin
-
-)
-
-router.post("/login", authController.login)
+const libraryStaffValidator = require("../middleware/libraryStaff/libraryStaffValidator");
+const libraryStaffControler = require("../controller/libraryStaff/libraryStaffController");
+const libraryStaffService = require("../service/libraryStaffService/libraryStaffService");
 
 router.post(
-    "/accept_requested_book",
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    LibraryAdminController.acceptRequestedBook
-    )
+  "/create",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdmiinValidator.validate(),
+  libraryAdmiinValidator.validationHnadler,
+  libraryAdminController.createAdmin
+);
+
+router.post("/login", authController.login);
+
+router.get(
+  "/find/requested-books/:userId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.findUserRequestedBook
+);
+router.post(
+  "/accept_requested_book",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  LibraryAdminController.acceptRequestedBook
+);
 
 // Create library staff
 router.post(
-    "/create/staff",
-    
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    libraryStaffValidator.validate(), 
-    libraryStaffValidator.validationHandler,
-    libraryStaffControler.register
-    )
+  "/create/staff",
+
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryStaffValidator.validate(),
+  libraryStaffValidator.validationHandler,
+  libraryStaffControler.register
+);
 router.get(
-    "/find/staff",
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    libraryAdminController.findLibraryStaff
-    )
+  "/find/staff",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.findLibraryStaff
+);
 router.patch(
-    "/update/staff/:staffId",
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    libraryAdminController.updateLibraryStaff
-    )
+  "/update/staff/:staffId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.updateLibraryStaff
+);
 router.delete(
-    "/delete/staff/:staffId", 
-    authenticateMiddleware.authenticate,
-    adminAuthorization.authorized,
-    libraryAdminController.deleteLibraryStaff
-    )
-module.exports = router
+  "/delete/staff/:staffId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.deleteLibraryStaff
+);
+router.get(
+  "/find-fine/:userId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.findUserFine
+);
+router.post(
+  "/pay-fine",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.payBookFine
+);
+router.get(
+  "/find-return-request/:userId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.findReturnRequest
+);
+
+router.post(
+  "/accept-return-request/:userId",
+  authenticateMiddleware.authenticate,
+  adminAuthorization.authorized,
+  libraryAdminController.acceptReturnBookRequest
+);
+module.exports = router;
