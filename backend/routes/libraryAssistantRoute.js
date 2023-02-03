3e85a1725b4380550f9d0149aef277035a0db405
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-02-03 08:56:10 +0530

    format librar assistant route.

--- backend/routes/libraryAssistantRoute.js
+++ backend/routes/libraryAssistantRoute.js
@@ -1,10 +1,17 @@
-const router = require("express").Router()
-const authController  = require("../controller/auth/teacherStudent/tsAuthController")
-const libraryAdminController = require("../controller/libraryAdmin/libraryAdminController")
-const LibraryAdminController = require("../controller/libraryAdmin/libraryAdminController")
-const authenticateMiddleware = require("../middleware/authenticate/authenticate")
-const acceptBookRequestAuthorization = require("../middleware/authorization/acceptBookRequestAuthorization")
-router.post("/login",authController.login)
-router.post("/accept_requested_book",authenticateMiddleware.authenticate, acceptBookRequestAuthorization.authorized, libraryAdminController.acceptRequestedBook   )
+const router = require("express").Router();
+const authController = require("../controller/auth/teacherStudent/tsAuthController");
+const libraryAdminController = require("../controller/libraryAdmin/libraryAdminController");
+const LibraryAdminController = require("../controller/libraryAdmin/libraryAdminController");
+const authenticateMiddleware = require("../middleware/authenticate/authenticate");
+const acceptBookRequestAuthorization = require("../middleware/authorization/acceptBookRequestAuthorization");
 
-module.exports = router
+router.post("/login", authController.login);
+
+router.post(
+  "/accept_requested_book",
+  authenticateMiddleware.authenticate,
+  acceptBookRequestAuthorization.authorized,
+  libraryAdminController.acceptRequestedBook
+);
+
+module.exports = router;
