0189219361d0fb2e043daf0f105155a7d1f1b31a
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-02-18 10:15:28 +0530

    Comment added for better understanding whole code.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const libraryAdminRoute =require("./libraryAdminRoute")
+const libraryAdminRoute = require("./libraryAdminRoute")
 const libraryCardRoute = require("./libraryCard");
 const studentAuthRoute = require("./studentAuth");
 const studentRoute = require("./studentRoute")
@@ -15,25 +15,32 @@ const authenticateMiddleware = require("../middleware/authenticate/authenticate"
 
 
 // Admin Routes
-router.use("/api/v1/libAdmin", libraryAdminRoute )
+router.use("/api/v1/libAdmin", libraryAdminRoute)
 
-
+// Library Card Route
 router.use("/api/v1/libraryCard", authenticateMiddleware.authenticate, libraryCardRoute);
 
+// Student Route
 router.use("/api/v1/auth/student", studentAuthRoute);
-router.use("/api/v1/student",authenticateMiddleware.authenticate, studentRoute)
+router.use("/api/v1/student", authenticateMiddleware.authenticate, studentRoute)
 
+// Teacher Route
 router.use("/api/v1/auth/teacher", teacherAuthRoute);
-router.use("/api/v1/teacher",authenticateMiddleware.authenticate, teacherRoute)
+router.use("/api/v1/teacher", authenticateMiddleware.authenticate, teacherRoute)
 
+// Library Assistant Route
 router.use("/api/v1/libAssistant", libraryAssistantRoute)
 
-router.use("/api/v1/book",authenticateMiddleware.authenticate, bookRoute);
+// Book Route
+router.use("/api/v1/book", authenticateMiddleware.authenticate, bookRoute);
 
-router.use("/api/v1/bookRequest",authenticateMiddleware.authenticate,  bookRequestRoute);
+// Book Request Route
+router.use("/api/v1/bookRequest", authenticateMiddleware.authenticate, bookRequestRoute);
 
-router.use("/api/v1/profile",authenticateMiddleware.authenticate, profileRouter )
+// Profile Route
+router.use("/api/v1/profile", authenticateMiddleware.authenticate, profileRouter)
 
+// Password Route
 router.use("/api/v1/password", authenticateMiddleware.authenticate, passwordRouter)
 
 module.exports = router;
