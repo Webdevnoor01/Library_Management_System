0e7784816b43c23a956704f937995ef0bce2014f
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-02-18 10:15:01 +0530

    Update book request route.

--- backend/routes/bookRequestRoute.js
+++ backend/routes/bookRequestRoute.js
@@ -1,17 +1,17 @@
 const bookRequestController = require("../controller/bookRequest/bookRequestController");
-const fequestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization");
+const requestedBookAuthorization = require("../middleware/authorization/requestedBookAuthorization");
 
 const router = require("express").Router();
 
 router.post(
     "/",
-    fequestedBookAuthorization.authorized,
+    requestedBookAuthorization.authorized,
     bookRequestController.newBookRequest
 );
 router.get("/find", bookRequestController.findRequestedBook);
 router.get(
     "/find/user/:userId",
-    fequestedBookAuthorization.authorized,
+    requestedBookAuthorization.authorized,
     bookRequestController.findUserRequestedBook
 );
 
