d25c571711ced91ec448b4c6d3a045c05a83e915
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-02-18 10:17:41 +0530

    Created a new service for library admin controller to to remove admin from the system.

--- backend/service/libraryAdminService/libraryAdminService.js
+++ backend/service/libraryAdminService/libraryAdminService.js
@@ -1,82 +1,118 @@
-const LibraryAdmin = require("../../models/lAdmin")
-const IssuedBook = require("../../models/issuedBook")
-const Fine = require("../../models/fine")
-const createError = require("http-errors")
-
-
+const LibraryAdmin = require("../../models/lAdmin");
+const IssuedBook = require("../../models/issuedBook");
+const Fine = require("../../models/fine");
+const createError = require("http-errors");
 
 class LibraryAdminService {
-    async create(payload){
+    async create(payload) {
         try {
-            const libAdmin = await LibraryAdmin.create(payload)
-            if(!libAdmin){
+            const libAdmin = await LibraryAdmin.create(payload);
+            if (!libAdmin) {
                 return {
-                    error:true,
-                    message:libAdmin
-                }
+                    error: true,
+                    message: libAdmin,
+                };
             }
             return {
-                error:false,
-                data:libAdmin
-            }
+                error: false,
+                data: libAdmin,
+            };
         } catch (e) {
-            throw createError(e.message)
+            throw createError(e.message);
         }
     }
 
-    async findByProperty(query){
+    async findByProperty(query) {
         try {
             let libAdmin;
-            if(query._id){
-                libAdmin = await LibraryAdmin.findById(query._id)
-                if(!libAdmin){
+            if (query._id) {
+                libAdmin = await LibraryAdmin.findById(query._id);
+                if (!libAdmin) {
                     return {
-                        error:true,
-                        message:libAdmin
-                    }
+                        error: true,
+                        message: "library admin not found",
+                    };
                 }
                 return {
-                    error:false,
-                    message:libAdmin
-                }
+                    error: false,
+                    message: libAdmin,
+                };
             }
-            libAdmin = await LibraryAdmin.findOne(query)
-            if(!libAdmin){
+            libAdmin = await LibraryAdmin.findOne(query);
+            if (!libAdmin) {
                 return {
-                    error:true,
-                    message:libAdmin
-                }
+                    error: true,
+                    message: "library admin not found",
+                };
             }
             return {
-                error:false,
-                message:libAdmin
-            }
+                error: false,
+                message: libAdmin,
+            };
         } catch (e) {
-            console.log(e)
-            throw createError(e.message)
+            console.log(e);
+            throw createError(e.message);
         }
     }
 
-    async findUserFine(query){
+    async findAll() {
         try {
-            const userFine =await Fine.find(query) 
-            if(userFine.length=== 0){
+            let libAdmin = await LibraryAdmin.find({});
+            console.log("libAdmin: ", libAdmin)
+            if (libAdmin.length === 0) {
                 return {
-                    error:true,
-                    message:"You don't have any fine"
-                }
+                    error: true,
+                    message: "empty library admin",
+                };
             }
             return {
-                error:false,
-                data:userFine
-            }
+                error: false,
+                data: libAdmin,
+            };
         } catch (e) {
-            console.log("findUserFine-libAdminService: ", e)
-            throw createError(e.message)
+            console.log(e);
+            throw createError(e.message);
         }
     }
 
-   
+    async findUserFine(query) {
+        try {
+            const userFine = await Fine.find(query);
+            if (userFine.length === 0) {
+                return {
+                    error: true,
+                    message: "You don't have any fine",
+                };
+            }
+            return {
+                error: false,
+                data: userFine,
+            };
+        } catch (e) {
+            console.log("findUserFine-libAdminService: ", e);
+            throw createError(e.message);
+        }
+    }
+
+    async delete(payload) {
+        try {
+            const admin = await LibraryAdmin.deleteOne(payload);
+            if (!admin) {
+                return {
+                    error: true,
+                    message: "Error to delete library admin",
+                };
+            }
+
+            return {
+                error: false,
+                data: admin,
+            };
+        } catch (e) {
+            console.log(e);
+            throw createError(e.message);
+        }
+    }
 }
 
-module.exports = new LibraryAdminService()
+module.exports = new LibraryAdminService();
